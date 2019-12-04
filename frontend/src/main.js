f8dfa97c037d33299d20ec3cf9c6e34140d03886
nghianv-0795 <ngo.van.nghia@sun-asterisk.com>
2019-12-04 10:13:49 +0700

    design-home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import VModal from 'vue-js-modal';
 import { router } from './router';
 
+Vue.use(VModal);
 Vue.config.productionTip = false;
 
 new Vue({
--- frontend/src/store/modules/contract.js
+++ frontend/src/store/modules/contract.js
@@ -1,18 +1,19 @@
 /* eslint-disable no-console */
-// import getWeb3 from '@/utils/getWeb3';
+import getWeb3 from '@/utils/getWeb3';
 const state = {
   web3: null
 };
 
 const mutations = {
   setWeb3(state, payload) {
+    state.web3 = null;
     state.web3 = payload;
   }
 };
 
 const actions = {
-  async setWeb3({ commit }, web3) {
-    commit('setWeb3', web3);
+  async setWeb3({ commit }) {
+    commit('setWeb3', await getWeb3());
   }
 };
 
