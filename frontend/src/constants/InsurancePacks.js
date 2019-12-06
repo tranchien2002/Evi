d57da0c109fd0ad4922955ab39be06cce94764fa
tran.minh.chien <SUN-ASTERISK\tran.minh.chien@b121716-pc.sun-asterisk.com>
2019-12-06 08:52:03 +0700

    feat: add components myEvi and InsurancePack

---
+++ frontend/src/constants/InsurancePacks.js
@@ -0,0 +1,28 @@
+const List = [
+  {
+    type: 'Basic',
+    price: 1000,
+    compensation: 3000,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscingelit. Integer et fringilla orci.',
+    logo: 'finiance-icon-03.png'
+  },
+  {
+    type: 'Medium',
+    price: 5000,
+    compensation: 20000,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscingelit. Integer et fringilla orci.',
+    logo: 'finiance-icon-02.png'
+  },
+  {
+    type: 'Premium',
+    price: 10000,
+    compensation: 50000,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscingelit. Integer et fringilla orci.',
+    logo: 'finiance-icon-01.png'
+  }
+];
+
+export default List;
